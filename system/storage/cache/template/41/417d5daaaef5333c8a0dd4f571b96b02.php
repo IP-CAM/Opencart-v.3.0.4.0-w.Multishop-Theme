ca1e823f36d1eb95058a34be9b4c026895d9d7e4
Aleksei-chance <aleksei.chance@gmail.com>
2025-01-08 20:13:36 +0300

    home page

---
+++ system/storage/cache/template/41/417d5daaaef5333c8a0dd4f571b96b02.php
@@ -0,0 +1,58 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* multishop/template/common/footer.twig */
+class __TwigTemplate_4c1537e0f0497935728312b0ede87a56 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "multishop/template/common/footer.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "multishop/template/common/footer.twig", "");
+    }
+}
